125fc74a8b709e5b4eff4d9f170afbe50ef8c9c9
Behshad Ghorbani <behshad.ghorbani@truescape.com>
2020-09-03 14:58:09 +1200

    login and logout api added

--- server/accounts/views.py
+++ server/accounts/views.py
@@ -4,6 +4,10 @@ from rest_framework.response import Response
 from knox.models import AuthToken
 from .serializers import UserSerializer, RegisterSerializer
 
+from django.contrib.auth import login
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from knox.views import LoginView as KnoxLoginView
+
 # Register API
 class RegisterAPI(generics.GenericAPIView):
     serializer_class = RegisterSerializer
@@ -17,3 +21,12 @@ class RegisterAPI(generics.GenericAPIView):
         "token": AuthToken.objects.create(user)[1]
         })
 
+class LoginAPI(KnoxLoginView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = AuthTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return super(LoginAPI, self).post(request, format=None)
--- server/urls.py
+++ server/urls.py
@@ -4,6 +4,9 @@ from server.things import views
 from rest_framework import routers
 from server.accounts.views import RegisterAPI
 
+from knox import views as knox_views
+from server.accounts.views import LoginAPI
+
 router = routers.DefaultRouter()
 router.register(r'things', views.ThingViewSet)
 
@@ -11,5 +14,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api/register/', RegisterAPI.as_view(), name='register'),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/login/', LoginAPI.as_view(), name='login'),
+    path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
 ]
